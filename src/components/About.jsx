7ec12cc117f00f80737a3ca4c37f9b6ad020e1e1
Dineth9D <dmuthusara@gmail.com>
2023-12-10 20:07:52 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        As an Artificial Intelligence (AI) Engineer with a strong foundation in programming, data analysis, and problem-solving, I specialize in Machine Learning (ML), Deep Learning, Natural Language Processing (NLP), Neural Networks, and Computer Vision. I have developed  AI applications for Robotics, Computer Vision, and Natural Language Processing, as well as applications using Cognitive Computing, Expert Systems, and Intelligent Agents. My experience in Pattern Recognition and Speech Recognition has allowed me to build Chatbots and Virtual Assistants that leverage Natural Language Processing and Machine Learning algorithms. My experience in these areas has allowed me to tackle complex problems and create innovative solutions. I am driven by my passion for AI and my desire to push the boundaries of what's possible.
+        <br />
+        <br />
+        Let's connect and explore how we can collaborate to make a meaningful impact in the world of AI.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
